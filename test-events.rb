eb82c10205aac1289d07477705df960cfdd19428
Ken Barber <ken@bob.sh>
2011-12-20 01:24:57 +0000

    Get matching working.

--- test-events.rb
+++ test-events.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env jruby -J-Dlog4j.configuration=log4j.xml
 
 require 'java'
 require 'lib/esper-4.4.0.jar'
@@ -9,8 +9,10 @@ require 'pp'
 
 # Turn on debugging
 cfg = com.espertech.esper.client.Configuration.new
-cfg.getEngineDefaults.getLogging.setEnableExecutionDebug(true)
-cfg.getEngineDefaults.getLogging.setEnableTimerDebug(true)
+logging_cfg = cfg.getEngineDefaults.getLogging
+logging_cfg.setEnableExecutionDebug(true)
+logging_cfg.setEnableTimerDebug(true)
+logging_cfg.setEnableQueryPlan(true)
 
 # Lets get the epService provider
 epService = com.espertech.esper.client.EPServiceProviderManager.getDefaultProvider()
@@ -20,36 +22,45 @@ configuration = epService.getEPAdministrator.getConfiguration
 
 # Prepare the OrderEvent type
 order_event_type = java.util.HashMap.new({"itemName" => "string",
-  "price" => "int"})
+  "price" => "double"})
 configuration.addEventType("OrderEvent", order_event_type)
 
 # Create EPL expression
 expression = "select avg(price) from OrderEvent.win:time(30 sec)";
-#expression = "select * from OrderEvent.win:time(30 sec)";
 statement = epService.getEPAdministrator.createEPL(expression)
 
 # Create a listener object
 class MyListener
-    include com.espertech.esper.client.UpdateListener
+  include com.espertech.esper.client.UpdateListener
 
-    java_signature 'public void update(EventBean[] newEvents, EventBean[] oldEvents)'
-    def update(newEvents, oldEvents)
-        event = newEvents[0]
-        puts event.inspect
-        puts("avg=" + event.get("avg(price)"))
-    end
+  java_signature 'public void update(EventBean[] newEvents, EventBean[] oldEvents)'
+  def update(newEvents, oldEvents)
+    event = newEvents[0]
+    puts("Matched average event avg=" + event.get("avg(price)").to_s)
+  end
 end
 
 # Register listener to statement
 listener = MyListener.new
 statement.addListener(listener);
 
-# Create an event
-order_event = {
-  "itemName" => "test",
-  "price" => 300,
-}
-order_event_java = java.util.HashMap.new(order_event)
+# Create an unmatched listener
+class MyUnmatchedListener
+  include com.espertech.esper.client.UnmatchedListener
+
+  java_signature 'public void update(EventBean[] event)'
+  def update(event)
+    puts "unmatched"
+    puts event.inspect
+  end
+end
+
+# Register unmatched listener
+unmatched_listener = MyUnmatchedListener.new
+epService.getEPRuntime.setUnmatchedListener(unmatched_listener)
 
 # And finally process the event
-epService.getEPRuntime.sendEvent(order_event_java)
+eprRuntime = epService.getEPRuntime
+eprRuntime.sendEvent({"itemName"=>"test","price"=>100}, "OrderEvent")
+eprRuntime.sendEvent({"itemName"=>"test","price"=>200}, "OrderEvent")
+eprRuntime.sendEvent({"itemName"=>"test","price"=>300}, "OrderEvent")
